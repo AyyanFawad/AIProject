e74220758e53123aa3debedef3cc348aa434bfe0
AyyanFawad <77777333+AyyanFawad@users.noreply.github.com>
2022-12-06 02:22:37 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ import textbutton
 HEIGHT, WIDTH = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Moody")
+
+
+# Colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+LILAC = (200, 162, 200)
 
+# Game Variables
 FPS = 60
-# load button image
+menu_state = "main"
+
+# Music Player Buttons
 play_image = pygame.image.load("assets/play_button.png").convert_alpha()
 pause_image = pygame.image.load("assets/pause_button.png").convert_alpha()
 next_image = pygame.image.load("assets/next_button.png").convert_alpha()
@@ -19,7 +26,11 @@ play_button = button.Button(50, 100, play_image, 1)
 pause_button = button.Button(50, 200, pause_image, 1)
 next_button = button.Button(50, 300, next_image, 1)
 
-testerbutton = textbutton.Button("Hello", 70, 70, (300, 300), 6)
+# Main Menu Button
+musicplayer_button = textbutton.Button("Music Player", 150, 150, (300, 300), 6)
+
+# Back Button
+back_button = textbutton.Button("Back", 50, 50, (0, 0), 6)
 
 
 def draw_window():
@@ -28,22 +39,35 @@ def draw_window():
 
 
 def main():
+    global menu_state
+    WIN.fill(LILAC)
     clock = pygame.time.Clock()
     RUNNING = True
+
     while RUNNING:
         clock.tick(FPS)
-        if play_button.draw(WIN):
-            print("play what u ape")
-        if pause_button.draw(WIN):
-            print("pause what u ape")
-        if next_button.draw(WIN):
-            print("next to what u ape")
-        if testerbutton.draw(WIN):
-            print("tester button working")
+        # if play_button.draw(WIN):
+        #     print("play what u ape")
+        # if pause_button.draw(WIN):
+        #     print("pause what u ape")
+        # if next_button.draw(WIN):
+        #     print("next to what u ape")
+        WIN.fill(LILAC)
+        if menu_state == "player":
+            # Show music player
+            play_button.draw(WIN)
+            pause_button.draw(WIN)
+            next_button.draw(WIN)
+            if back_button.draw(WIN):
+                menu_state = "main"
+        elif menu_state == "main":
+            if musicplayer_button.draw(WIN):
+                menu_state = "player"
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
+
         pygame.display.update()
 
     pygame.quit()
